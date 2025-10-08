fec263b1addcce293e0ef11f3f336bfbe1c0ade9
Ved Patel <veds28112004@gmail.com>
2025-10-07 20:42:28 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  root: 'client',
+  plugins: [react()],
+  build: {
+    outDir: '../dist', // Adjust the output directory if needed
+  },
+})
