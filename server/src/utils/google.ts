60fdbffa4279089bc34dab3fad83032e4d390660
Ved Patel <veds28112004@gmail.com>
2025-10-15 19:47:45 -0400

    #4 issue solved

--- server/src/utils/google.ts
+++ server/src/utils/google.ts
@@ -1,8 +1,15 @@
 import { OAuth2Client } from "google-auth-library";
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-export async function verifyGoogleIDToken(idToken: string){
-    const ticket = await client.verifyIdToken({ idToken, audience: process.env.GOOGLE_CLIENT_ID });
-    const payload = ticket.getPayload();
-    return payload;
+
+export async function verifyGoogleIDToken(idToken: string) {
+  const audience = process.env.GOOGLE_CLIENT_ID;
+  if (!audience) throw new Error("Missing GOOGLE_CLIENT_ID environment variable");
+
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience,
+  });
+
+  return ticket.getPayload();
 }
